b892a4b17d6ee6fc58a98a149cb3b1b3683dc832
ZarwinVillaro <133875086+ZarwinVillaro@users.noreply.github.com>
2024-06-20 07:41:13 +0800

    Update sbc_d2_act2.py

--- sbc_d2_act2.py
+++ sbc_d2_act2.py
@@ -14,10 +14,10 @@ print(f"C2 = {c2} - RANDOM")
 print(f"C3 = {c3} - RANDOM")
 
 if input == c2 == c3:
-    print("It's a tie! Maiiba!")
-elif input < c2 or input == c3:
+    print("It's a tie!")
+elif input > c2 or input > c3:
     print("P1 WINS!")
-elif c2 == c3:
+elif input < c2:
     print("C2 WINS!")
 else:
     print("C3 WINS!")
